37aef85391472ee87c5d33efb74e6f8914b72334
Neil <h66r10@gmail.com>
2021-05-31 00:00:19 +0200

    old school

--- src/main/java/com/seedfinding/neil/Main.java
+++ src/main/java/com/seedfinding/neil/Main.java
@@ -1,16 +1,15 @@
 package com.seedfinding.neil;
 
-import kaptainwutax.featureutils.GenerationContext;
+import kaptainwutax.biomeutils.source.OverworldBiomeSource;
 import kaptainwutax.featureutils.structure.Mansion;
 import kaptainwutax.mcutils.rand.ChunkRand;
-import kaptainwutax.mcutils.rand.seed.WorldSeed;
+import kaptainwutax.mcutils.rand.seed.StructureSeed;
 import kaptainwutax.mcutils.util.pos.BPos;
 import kaptainwutax.mcutils.util.pos.CPos;
 import kaptainwutax.mcutils.util.pos.RPos;
 import kaptainwutax.mcutils.version.MCVersion;
+import kaptainwutax.terrainutils.terrain.OverworldTerrainGenerator;
 
-import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 
 public class Main {
@@ -27,19 +26,18 @@ public class Main {
 		RPos maxBound = new BPos(3000, 0, 3000).toRegionPos(mansion.getSpacing() * 16);
 		for (int regX = minBound.getX() + 1; regX <= maxBound.getX(); regX++) {
 			for (int regZ = minBound.getZ() + 1; regZ < maxBound.getZ(); regZ++) {
-				CPos pos = mansion.getInRegion(structureSeed, regX, regZ, rand);
-				BPos bPos = pos.toBlockPos().add(9, 0, 9);
-				List<GenerationContext.Context> list = WorldSeed.getSisterSeeds(structureSeed).asStream()
-						.mapToObj(mansion::getContext)
-						.filter(c -> mansion.canSpawn(pos, c.getBiomeSource()))
-						.collect(Collectors.toList());
-				if (!list.isEmpty()) {
-					GenerationContext.Context context = list.get(0);
-					int y = context.getGenerator().getHeightOnGround(bPos.getX(), bPos.getZ());
-					if (y > 90) {
-						System.out.printf("y=%d, Found %s at /tp @p %d ~ %d with y=%d%n", y, context.getWorldSeed(), bPos.getX(), bPos.getZ(), y);
-						for (GenerationContext.Context c : list) {
-							System.out.printf("Valid also :  Found %s at /tp @p %d ~ %d%n", context.getWorldSeed(), bPos.getX(), bPos.getZ());
+				CPos cpos = mansion.getInRegion(structureSeed, regX, regZ, rand);
+				BPos bPos = cpos.toBlockPos().add(9, 0, 9);
+				for (int upper = 0; upper < 1 << 16L; upper++) {
+					long ws = StructureSeed.toWorldSeed(structureSeed, upper);
+					OverworldBiomeSource overworldBiomeSource = new OverworldBiomeSource(VERSION, ws);
+					if (mansion.canSpawn(cpos, overworldBiomeSource)) {
+						OverworldTerrainGenerator terrainGenerator = new OverworldTerrainGenerator(overworldBiomeSource);
+						int y = terrainGenerator.getHeightOnGround(bPos.getX(), bPos.getZ());
+						if (y > 90) {
+							System.out.printf("y=%d, Found %s at /tp @p %d ~ %d with y=%d%n", y, ws, bPos.getX(), bPos.getZ(), y);
+							System.out.println("SAVE %s" + structureSeed);
+							break;
 						}
 					}
 				}
